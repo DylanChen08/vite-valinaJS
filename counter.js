e0ee61b5ba4e3149aa5c97dd091a02540a908157
cy <3205561979@qq.com>
2023-04-17 09:28:14 +0800

    feature: 1. instance of

--- counter.js
+++ counter.js
@@ -1,11 +1,11 @@
 export function setupCounter(element) {
-  let counter = 0
-  const setCounter = (count) => {
-    counter = count
-    element.innerHTML = `count is ${counter}`
-  }
-  element.addEventListener('click', () => setCounter(counter + 1))
-  setCounter(0)
+    let counter = 0
+    const setCounter = (count) => {
+        counter = count
+        element.innerHTML = `count is ${counter}`
+    }
+    element.addEventListener('click', () => setCounter(counter + 1))
+    setCounter(0)
 }
 
 
@@ -19,7 +19,6 @@ export function setupCounter(element) {
 //       // set.add(JSON.stringify(item))
 //       item = JSON.stringify(item)
 //     }
-//     //todo
 //     if (set.has(item)) return false
 //     set.add(item)
 //     return true
@@ -38,7 +37,6 @@ export function setupCounter(element) {
 // // map.set({a:2},[11,11,13])
 // console.log(typeof map)
 // console.log(map.has(o2));
-
 
 
 //循环引用
@@ -72,13 +70,13 @@ export function setupCounter(element) {
 // function deepCopyFunction(fn) {
 //   return fn.bind(null)
 // }
-
+//
 // function sum(a, b) {
 //   return a + b
 // }
 // let sum2 = deepCopyFunction(sum)
 // console.log( sum2(3, 4) )
-
+//
 // sum2.prototype =  function clear (params) {
 //   console.log('clear')
 // }
@@ -91,8 +89,6 @@ export function setupCounter(element) {
 // // let str = '\n \t jirengu  \t'
 // let str = '  woa'
 // console.log( trim(str) )
-
-
 
 
 // function sum(a, b, c, d) {
@@ -114,7 +110,6 @@ export function setupCounter(element) {
 // let newSum  = curry(sum)
 // // newSum(1)(2)(3,4)(5)
 // newSum(1,2,3,4)
-
 
 
 // function curry(fn) {
@@ -149,80 +144,128 @@ export function setupCounter(element) {
 // console.log( curry(getUrl)('jirengu.com')('query=vue2') )
 
 
+// class EventBus {
+//   events = new Map()
+//
+//   on(eventType, handler) {
+//     //this.events.get(eventType)已经是 new Set() ，往Set 里面加入 handler
+//     //set 性能比数组好，因为 Set 无次序
+//     if (!this.events.has(eventType)) {
+//       //如果eventType里面不含有events的类型，那么我们就在map里面新增一个 Set()
+//       this.events.set(eventType, new Set())
+//     }
+//     handler.type = 'on'
+//     //如果eventType里面含有events的类型，那么我们就在map里面添加
+//     this.events.get(eventType).add(handler)
+//
+//   }
+//   emit(eventType, data) {
+//     if (this.events.has(eventType)) {
+//       // 遍历eventType里面的handler
+//       this.events.get(eventType).forEach(handler => {
+//         // console.log(handler)
+//         handler(data)
+//
+//         if (handler.type === 'once') {
+//           this.events.get(eventType).delete(handler)
+//
+//         }
+//       })
+//     }
+//   }
+//
+//   off(eventType, handler) {
+//     if (this.events.has(eventType)) {
+//       // 如果不存在这个handler,则直接清除events里面该类型。
+//       if (!handler) {
+//         console.log(this.events, 'this.events')
+//         this.events.delete(eventType)
+//       } else {
+//         // set里面删除
+//         this.events.get(eventType).delete(handler)
+//       }
+//     }
+//   }
+//
+//   once(eventType, handler) {
+//     if(!this.events.has(eventType)){
+//       this.events.set(eventType, new Set())
+//     }
+//     handler.type = 'once'
+//     this.events.get(eventType).add(handler)
+//   }
+//
+//
+//
+// }
+//
+// const bus = new EventBus()
+// console.log(bus)
+// bus.on('click', (data) => {
+//   console.log('clicked, data: ', data)
+// })
+// bus.on('click', (data) => {
+//   console.log('clicked2, data: ', data)
+// })
+//
+// bus.emit('click', { a: 1 })
+// bus.emit('click', { a: 2 })
+//
+// bus.once('hello', (data) => {
+//   console.log('say hello, data: ', data)
+// })
+//
+// bus.emit('hello', 'dcwonderland')
+// bus.emit('hello', 'frontend')
+//
+// bus.off('click')
+// bus.emit('click', {a: 1})
 
-class EventBus {
-  events = new Map()
 
-  on(eventType, handler) {
-    //this.events.get(eventType)已经是 new Set() ，往Set 里面加入 handler
-    //set 性能比数组好，因为 Set 无次序
-    if (!this.events.has(eventType)) {
-      //如果eventType里面不含有events的类型，那么我们就在map里面新增一个 Set()
-      this.events.set(eventType, new Set())
-    }
-    handler.type = 'on'
-    //如果eventType里面含有events的类型，那么我们就在map里面添加
-    this.events.get(eventType).add(handler)
+//instanceOf
 
-  }
-  emit(eventType, data) {
-    if (this.events.has(eventType)) {
-      // 遍历eventType里面的handler
-      this.events.get(eventType).forEach(handler => {
-        // console.log(handler)
-        handler(data)
+// say() 先从自身properties去寻找 找不到的话从自己的原型上去找 再找不到从原型链去找
 
-        if (handler.type === 'once') {
-          this.events.get(eventType).delete(handler)
+// 判断 c.__proto__ === Cat.prototype?是实例:否
 
+
+// function myinstanceof(instance, func) {
+//   let __proto__ = Object.getPrototypeOf(instance)
+//   while(__proto__) {
+//     debugger
+//     if(__proto__ === func.prototype) {
+//       return true
+//     } else {
+//       __proto__ = Object.getPrototypeOf(__proto__)
+//     }
+//   }
+//   return false
+// }
+//
+// function Cat() {}
+// let c = new Cat()
+// console.log(myinstanceof(c, Cat))
+
+
+//手写一个instanceof
+function myinstanceof(instance, func) {
+    let __proto__ = instance.__proto__
+    while (__proto__) {
+        if (__proto__ === func.prototype) {
+            //循环判断，一直找到func的原型构造,如果存在就返回true
+            return true
+        } else {
+            //否则就把当前实例的原型构造返回去
+            __proto__ = __proto__.__proto__
         }
-      })
     }
-  }
-
-  off(eventType, handler) {
-    if (this.events.has(eventType)) {
-      // 如果不存在这个handler,则直接清除events里面该类型。
-      if (!handler) {
-        console.log(this.events, 'this.events')
-        this.events.delete(eventType)
-      } else {
-        // set里面删除
-        this.events.get(eventType).delete(handler)
-      }
-    }
-  }
-
-  once(eventType, handler) {
-    if(!this.events.has(eventType)){
-      this.events.set(eventType, new Set())
-    }
-    handler.type = 'once'
-    this.events.get(eventType).add(handler)
-  }
 
 
-
+    return instance.__proto__ === func.prototype
 }
 
-const bus = new EventBus()
-console.log(bus)
-bus.on('click', (data) => {
-  console.log('clicked, data: ', data)
-})
-bus.on('click', (data) => {
-  console.log('clicked2, data: ', data)
-})
+function Cat() {
+}
 
-bus.emit('click', { a: 1 })
-bus.emit('click', { a: 2 })
-
-bus.once('hello', (data) => {
-  console.log('say hello, data: ', data)
-})
-
-bus.emit('hello', 'dcwonderland')
-bus.emit('hello', 'frontend')
-
-bus.off('click')
-bus.emit('click', {a: 1})
+let c = new Cat()
+console.log(myinstanceof(c, Object))
