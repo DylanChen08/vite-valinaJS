209413ebd605264f9eac46ccd648555dee8805b9
DC <easetreychen@gmail.com>
2023-05-15 23:54:35 +0800

    try catch

--- src/forEach.js
+++ src/forEach.js
@@ -41,31 +41,46 @@ export function ff(params) {
 
 //reduce 
 
-Array.prototype._reduce = function (callback,inintialValue) {
-    //如果传入的数组为空,抛出异常
-    if(this.length===0){
-        if(inintialValue!==undefined){
-            return inintialValue
-        }else{
-            throw new TypeError('Reduce of empty array wiht no initial value')
-        }
-    }
-    let previousValue 
-    //处理没有初始值的情况
-    if(inintialValue===undefined){
-        previousValue =  this[0]
-    }else{
-        previousValue = callback(inintialValue,this[0])
-    }
-    for (let i = 0; i < array.length; i++) {
-        previousValue = callback(previousValue,this[i])
-    }
-    return previousValue
-}
+// Array.prototype._reduce = function (callback,inintialValue) {
+//     //如果传入的数组为空,抛出异常
+//     if(this.length===0){
+//         if(inintialValue!==undefined){
+//             return inintialValue
+//         }else{
+//             throw new TypeError('Reduce of empty array wiht no initial value')
+//         }
+//     }
+//     let previousValue 
+//     //处理没有初始值的情况
+//     if(inintialValue===undefined){
+//         previousValue =  this[0]
+//     }else{
+//         previousValue = callback(inintialValue,this[0])
+//     }
+//     for (let i = 0; i < array.length; i++) {
+//         previousValue = callback(previousValue,this[i])
+//     }
+//     return previousValue
+// }
 
-let a  = []
+// let a  = []
 
 // console.log(a._filter(c=>c>=0))
 
-console.log(a._reduce((v1,v2)=>v1+v2,100))
 // console.log(a._reduce((v1,v2)=>v1+v2,100))
+// console.log(a._reduce((v1,v2)=>v1+v2,100))
+
+
+
+try {
+    // nonExistentFunction();
+    let a = 0 
+    console.log(b)
+   
+  } catch (error) {
+    // Object.keys(error).forEach(v=>{console.log(v)})
+    console.log(error?'true':'fa');
+    // Expected output: ReferenceError: nonExistentFunction is not defined
+    // (Note: the exact output may be browser-dependent)
+  }
+  
