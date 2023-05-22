2b61cc7137a859d7d5ed7f072f53a1310d74327d
cy <3205561979@qq.com>
2023-05-22 15:13:59 +0800

    feature: 1. promise.all

--- src/myPromise.js
+++ src/myPromise.js
@@ -4,22 +4,30 @@ export const pr = () => {
 
 // console.log('promise')
 
+// Promise + Fetch 封装一个 ajax :
+// const ajax = (url = '', method = 'GET', data = {}) => {
+//     return new Promise((resolve, reject) => {
+//         let options = {method}
+//         if (method === 'GET') {
+//             // 把类似 {name:dylan,favourite:beef} 转化为 ?name=dylan&&favourite=beef
+//             url += '?' + Object.entries(data).map(arr => arr[0] + '=' + arr[1]).join('&')
+//         } else if (method === 'POST') {
+//             options.body = JSON.stringify(data)
+//             options.headers = {'Content-Type': 'application/json'}
+//         }
+//         fetch(url, options).then(res => res.json())
+//             .then(data => resolve(data))
+//             .catch(e => reject(e))
+//     })
+// }
+//
+// ajax('https://www.baidu.com', "GET", {name: 1, id: 1, content: 1})
+//     .then(r => {
+//         console.log(r)
+//     })
+//     .catch(err => {
+//         console.log(err)
+//     })
 
-function ajax(url = '', method = 'GET', data = {}) {
-    return new Promise((resolve, reject) => {
-        let options = {method}
-        if (method === 'GET') {
-            url += '?' + Object.entries(data).map(arr => arr[0] + '=' + arr[1]).join('&')
-        } else if (method === 'POST') {
-            options.body = JSON.stringify(data)
-            options.headers = {'Content-Type': 'application/json'}
-        }
-        fetch(url, options).then(res => res.json())
-            .then(data => resolve(data))
-            .catch(e => reject(e))
-    })
-}
+// promise.all
 
-ajax('https://localhost:8080', "GET", '{name:1,id:1,content:1}').then(r => {
-    console.log(r)
-})
